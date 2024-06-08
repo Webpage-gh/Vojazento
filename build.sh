312393d6c6d6d65576d90ab4ca6c336b47ff3b3c
Webpage <70434775+Webpage-gh@users.noreply.github.com>
2024-06-08 16:39:48 +0800

    Build two versions

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 APK_PATH="app/build/outputs/apk/release/app-release-unsigned.apk"
 JAVA_FILE="app/src/main/java/com/sevtinge/vojazento/XposedInit.java"
@@ -7,10 +8,11 @@ KEYWORD="boolean IS_CHINA_MAINLAND_BUILD"
 build_and_move_apk() {
     local build_type=$1
     ./gradlew build
-    mv "$APK_PATH" "$(dirname APK_PATH)/Vojazento-$build_type.apk"
+    mv "$APK_PATH" "release/Vojazento-$build_type.apk"
 }
 
-chmod +x ./gradle
+chmod +x ./gradlew
+mkdir -p release
 
 if grep -q "$KEYWORD = true" $JAVA_FILE; then
     build_and_move_apk "China"
